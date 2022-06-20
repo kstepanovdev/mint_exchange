97c9bb06daa9569d6716bf9ba6020962525760cf
Kyrylo Stepanov <k.stepanov@tutanota.com>
2022-06-20 11:23:50 +0300

    fixed: removed redundant imports and unused variables

--- app/src/MintNft.tsx
+++ app/src/MintNft.tsx
@@ -1,39 +1,15 @@
 import { 
-    // clusterApiUrl, 
-//     Connection, 
-//     PublicKey, 
     Keypair, 
-//     LAMPORTS_PER_SOL, 
     Transaction, 
     SystemProgram,
     PublicKey,
-    // TransactionInstruction
-//     sendAndConfirmTransaction, 
-//     TransactionInstruction
 } from '@solana/web3.js';
 import { 
     createAssociatedTokenAccountInstruction,
     createInitializeMintInstruction,
-    createMint, 
     getAssociatedTokenAddress, 
-    // mintTo, 
-    // Account, 
-    // createSetAuthorityInstruction, 
-    // AuthorityType,
-    // getOrCreateAssociatedTokenAccount
 } from '@solana/spl-token';
 import { programID } from "./utils/config";
-// import { 
-//     WalletDialogProvider 
-// } from "@solana/wallet-adapter-material-ui";
-// import { 
-//     PhantomWalletAdapter 
-// } from "@solana/wallet-adapter-wallets";
-// import {
-//     ConnectionProvider,
-//     WalletProvider,
-//     Wallet
-// } from "@solana/wallet-adapter-react";
 import idl from "./mint_exchange.json";
 
 import { WalletNotConnectedError } from '@solana/wallet-adapter-base';
@@ -44,15 +20,11 @@ import {
 } from '@solana/spl-token';
 import React, { FC, useCallback } from 'react';
 import { Program, AnchorProvider, getProvider, setProvider, Provider } from "@project-serum/anchor";
-// import { sendAndConfirmTransaction } from '@solana/web3.js';
-// import { WalletModalProvider } from '@solana/wallet-adapter-react-ui';
 
 export const MintNft: FC = () => {
     const mintKey = Keypair.generate();
     const { connection } = useConnection();
     const { wallet, publicKey, sendTransaction, signTransaction, signAllTransactions } = useWallet();
-    const kek = useWallet();
-    // const provider = new AnchorProvider(connection, kek, "processed"); 
     
     const onClick = useCallback(async () => {
         if (!wallet || !publicKey || !signTransaction || !signAllTransactions) throw new WalletNotConnectedError();
@@ -77,9 +49,7 @@ export const MintNft: FC = () => {
             MINT_SIZE
         );
         
-        // Fires a list of instructions
         const mint_tx = new Transaction().add(
-            // Use anchor to create an account from the mint key that we created
             SystemProgram.createAccount({
               fromPubkey: publicKey,
               newAccountPubkey: mintKey.publicKey,
@@ -87,11 +57,9 @@ export const MintNft: FC = () => {
               programId: TOKEN_PROGRAM_ID,
               lamports,
             }),
-            // Fire a transaction to create our mint account that is controlled by our anchor wallet
             createInitializeMintInstruction(
               mintKey.publicKey, 0, publicKey, publicKey, TOKEN_PROGRAM_ID
             ),
-            // Create the ATA account that is associated with our mint on our anchor wallet
             createAssociatedTokenAccountInstruction(
               publicKey, associatedTokenAccount, publicKey, mintKey.publicKey, TOKEN_PROGRAM_ID
             )
@@ -100,21 +68,9 @@ export const MintNft: FC = () => {
         mint_tx.feePayer = new PublicKey(publicKey);
         mint_tx.recentBlockhash = (await connection.getLatestBlockhash("finalized")).blockhash;
         mint_tx.partialSign(mintKey);
-        // console.log(mint_tx.recentBlockhash);
-        // await signTransaction(mint_tx);
-        // tx.recentBlockhash = await connection.getLatestBlockhash("finalized")).blockhash;
         await sendTransaction(mint_tx, connection);
         console.log(mintKey.publicKey);
 
-        // sends and create the transaction
-        // const res = await anchor.AnchorProvider.env().sendAndConfirm(mint_tx, [mintKey]);
-        // await sendAndConfirmTransaction(connection, mint_tx, [mintKey]);
-        // console.log(signature);
-        // const mint = await sendAndConfirmTransaction(connection, mint_tx, [mintKey]);
-                
-        // const mint = await createMint(connection, mintKey, publicKey, null, 0);
-        
-        // const program = new Program(idl, programID, provider);
         await program.methods.mintToken().accounts({
             mint: mintKey.publicKey,
             tokenProgram: TOKEN_PROGRAM_ID,
@@ -122,17 +78,6 @@ export const MintNft: FC = () => {
             authority: publicKey,
           }).rpc()
 
-        // const transaction = new Transaction().add(
-        //     SystemProgram.transfer({
-        //         fromPubkey: publicKey,
-        //         toPubkey: Keypair.generate().publicKey,
-        //         lamports: 1,
-        //     })
-        // );
-
-        // const signature = await sendTransaction(transaction, connection);
-
-        // await connection.confirmTransaction(signature, 'processed');
     }, [publicKey, sendTransaction, connection]);
 
     return (
--- programs/mint_exchange/src/lib.rs
+++ programs/mint_exchange/src/lib.rs
@@ -1,70 +1,7 @@
 use anchor_lang::prelude::*;
 use anchor_spl::token::{self, MintTo, Token};
-// use spl_associated_token_account::instruction;
-// use solana_program;
 
 declare_id!("CGEYAjkfYU696uy1i3XSpn5TzVvprPMTdAbsng9b2jrE");
-
-// #[program]
-// pub mod mint_exchange {
-//     use anchor_spl::token::Transfer;
-
-//     use super::*;
-
-//     pub fn buy_nft(ctx: Context<MintTrasfer>) -> Result<()> {
-//         // create ATA for a Wallet
-        
-//         let cpi_accounts = MintTo {
-//             mint: ctx.accounts.mint.to_account_info(),
-//             to: ctx.accounts.token_account.to_account_info(),
-//             authority: ctx.accounts.authority.to_account_info(),
-//         };
-        
-//         let cpi_program = ctx.accounts.token_program.to_account_info();
-//         // Create the CpiContext we need for the request
-//         let cpi_ctx = CpiContext::new(cpi_program, cpi_accounts);
-
-//         // Execute anchor's helper function to mint tokens
-//         token::mint_to(cpi_ctx, 10)?;
-        
-//         // let transfer_instruction = Transfer {
-//         //     from: ctx.accounts.mint.to_account_info(),
-//         //     to: ctx.accounts.token_account.to_account_info(),
-//         //     authority: ctx.accounts.wallet.to_account_info(),
-//         // };
-         
-//         // let cpi_program = ctx.accounts.token_program.to_account_info();
-//         // // // Create the Context for our Transfer request
-//         // let cpi_ctx = CpiContext::new(cpi_program, transfer_instruction);
-        
-//         // // // Execute anchor's helper function to transfer tokens
-//         // anchor_spl::token::transfer(cpi_ctx, 1)?;
-        
-//         Ok(())
-//     }
-// }
-
-// #[derive(Accounts)]
-// pub struct MintTrasfer<'info> {
-//     /// CHECK: add check here! 
-//     mint: AccountInfo<'info>,
-//     /// CHECK: add check here! 
-//     wallet: AccountInfo<'info>,
-//     /// CHECK: add check here! 
-//     token_program: Program<'info, Token>,
-//     /// CHECK: add check here! 
-//     token_account: AccountInfo<'info>,
-// }
-
-// impl<'info> MintTrasfer<'info> {
-//     fn ata_instruction(&self) -> Instruction {
-//         instruction::create_associated_token_account(
-//             self.wallet.key, 
-//             self.mint.key,
-//             self.token_program.key
-//         )
-//     }
-// }
 
 #[program]
 pub mod mint_exchange {
@@ -73,7 +10,6 @@ pub mod mint_exchange {
     use super::*;
 
     pub fn mint_token(ctx: Context<MintToken>,) -> Result<()> {
-        // Create the MintTo struct for our context
         let cpi_accounts = MintTo {
             mint: ctx.accounts.mint.to_account_info(),
             to: ctx.accounts.token_account.to_account_info(),
@@ -81,29 +17,9 @@ pub mod mint_exchange {
         };
 
         let cpi_program = ctx.accounts.token_program.to_account_info();
-        // Create the CpiContext we need for the request
         let cpi_ctx = CpiContext::new(cpi_program, cpi_accounts);
 
-        // Execute anchor's helper function to mint tokens
         token::mint_to(cpi_ctx, 1)?;
-
-        Ok(())
-    }
-
-    pub fn transfer_token(ctx: Context<TransferToken>) -> Result<()> {
-        // Create the Transfer struct for our context
-        let transfer_instruction = Transfer {
-            from: ctx.accounts.from.to_account_info(),
-            to: ctx.accounts.to.to_account_info(),
-            authority: ctx.accounts.from_authority.to_account_info(),
-        };
-
-        let cpi_program = ctx.accounts.token_program.to_account_info();
-        // Create the Context for our Transfer request
-        let cpi_ctx = CpiContext::new(cpi_program, transfer_instruction);
-
-        // Execute anchor's helper function to transfer tokens
-        anchor_spl::token::transfer(cpi_ctx, 1)?;
 
         Ok(())
     }
@@ -112,14 +28,14 @@ pub mod mint_exchange {
 
 #[derive(Accounts)]
 pub struct MintToken<'info> {
-    /// CHECK: This is the token that we want to mint
+    /// CHECK: Token 
     #[account(mut)]
     pub mint: AccountInfo<'info>,
     pub token_program: Program<'info, Token>,
-    /// CHECK: This is the token account that we want to mint tokens to
+    /// CHECK: Token account 
     #[account(mut)]
     pub token_account: UncheckedAccount<'info>,
-    /// CHECK: the authority of the mint account
+    /// CHECK: authority 
     #[account(mut)]
     pub authority: AccountInfo<'info>,
 }
@@ -127,12 +43,12 @@ pub struct MintToken<'info> {
 #[derive(Accounts)]
 pub struct TransferToken<'info> {
     pub token_program: Program<'info, Token>,
-    /// CHECK: The associated token account that we are transferring the token from
+    /// CHECK: ATA from 
     #[account(mut)]
     pub from: UncheckedAccount<'info>,
-    /// CHECK: The associated token account that we are transferring the token to
+    /// CHECK: ATA to 
     #[account(mut)]
     pub to: AccountInfo<'info>,
-    // the authority of the from account
+    // Authority from 
     pub from_authority: Signer<'info>,
 }
